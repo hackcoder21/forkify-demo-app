b555d9f2f1ca84e4b4b4415a502a4278b94ad60f
hackcoder21 <jadhavrajat1998@gmail.com>
2024-11-03 01:28:54 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//Variables that should be constants and reused
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '75f97851-079c-4b0a-9eb3-7bdde55dcc15';
+export const MODAL_CLOSE_SEC = 2.5;
