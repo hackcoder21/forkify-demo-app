465054ef5b91723fb7a65df2efcd3a6b24e89bd3
hackcoder21 <jadhavrajat1998@gmail.com>
2024-11-03 14:56:58 +0530

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,9 +10,9 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -128,10 +128,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -140,6 +136,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagniation);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
